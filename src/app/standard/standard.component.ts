3007785fa7ec4e1ef33e21c75954183384113611
austin <acmoudy@yahoo.com>
2026-08-16 13:28:58 -0400

    Added all operation function to calculator and clearing capabilities

--- src/app/standard/standard.component.ts
+++ src/app/standard/standard.component.ts
@@ -10,30 +10,91 @@ export class StandardComponent {
   displayValue: string = '0';
   decimal:boolean = false;
   operator: string | null = null;
+  firstValue: string | null = null;
+  secondValue: string | null = null;
+  operators: string[] = ['-','+','*','/'];
+  displayClear: boolean = true;
+  answer: number = 0;
 
 
   clearDisplay(){
     this.displayValue = '0';
-    this.decimal = false;
+    this.displayClear = true;
   }
 
-  clearHistory(){}
+  clearHistory(){
+    this.displayValue = '0';
+    this.displayClear = true;
+    this.answer = 0;
+    this.firstValue = null;
+    this.secondValue = null;
+    this.operator = null;
+  }
 
   appendNumber(val: string){
-    if(this.displayValue === '0'){ this.displayValue = val;
-    }else{
-    this.displayValue = this.displayValue + val;
+    if(this.operator == null && this.operators.includes(this.displayValue)){
+      this.operator = this.displayValue;
+      this.displayClear = true;
+    }
+
+    if(this.displayClear){
+      this.displayValue = val;
+      this.displayClear = false;
+    } else{
+    this.displayValue += val;
     }
   }
 
   appendDecimal(){
-    if(!this.decimal){
-      this.displayValue = this.displayValue + '.';
-      this.decimal = true;
+  }
+
+  setOperator(operator: string){
+    if(this.operator != null){
+      return;
+    }
+    if(this.firstValue == null){
+      this.firstValue = this.displayValue;
+      this.displayClear = true;
+    }
+    if(this.displayClear){
+      this.displayValue = operator
     }
   }
 
-  setOperator(operator: string){}
-
-  evaluate(){}
+  evaluate(){
+    if(this.firstValue == null || this.operator == null){
+      return;
+    }
+    this.secondValue = this.displayValue;
+    if(this.operator == '+'){
+      this.answer = parseFloat(this.firstValue) + parseFloat(this.secondValue);
+      this.displayValue = this.answer.toString();
+      this.displayClear = true;
+      this.operator = null;
+      this.firstValue = null;
+    }else if(this.operator == '-'){
+      this.answer = parseFloat(this.firstValue) - parseFloat(this.secondValue);
+      this.displayValue = this.answer.toString();
+      this.displayClear = true;
+      this.operator = null;
+      this.firstValue = null;
+    }else if(this.operator == '/'){
+      let div = parseFloat(this.secondValue);
+      if(div == 0){
+        this.answer = NaN;
+      }else{
+      this.answer = parseFloat(this.firstValue) / div;
+      }
+      this.displayValue = this.answer.toString();
+      this.displayClear = true;
+      this.operator = null;
+      this.firstValue = null;
+    }else if(this.operator == '*'){
+      this.answer = parseFloat(this.firstValue) * parseFloat(this.secondValue);
+      this.displayValue = this.answer.toString();
+      this.displayClear = true;
+      this.operator = null;
+      this.firstValue = null;
+    }
+  }
 }
