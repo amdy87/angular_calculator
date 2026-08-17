96ce5d1d3d032d4365bdf930cde4c75ef2cebdd8
austin <acmoudy@yahoo.com>
2026-08-17 11:11:52 -0400

    Added decimal functionality to the standard calculator

--- src/app/standard/standard.component.ts
+++ src/app/standard/standard.component.ts
@@ -17,11 +17,14 @@ export class StandardComponent {
   answer: number = 0;
 
 
+  //clears the display line 
   clearDisplay(){
     this.displayValue = '0';
     this.displayClear = true;
+    this.decimal = false;
   }
 
+  //clears the display line, first value, operator and second value
   clearHistory(){
     this.displayValue = '0';
     this.displayClear = true;
@@ -29,14 +32,18 @@ export class StandardComponent {
     this.firstValue = null;
     this.secondValue = null;
     this.operator = null;
+    this.decimal = false;
   }
 
+  //appends a number to the display line
   appendNumber(val: string){
+    //checks if the operator needs to be set from the display line
     if(this.operator == null && this.operators.includes(this.displayValue)){
       this.operator = this.displayValue;
       this.displayClear = true;
     }
 
+    //updates the display line with the number being typed
     if(this.displayClear){
       this.displayValue = val;
       this.displayClear = false;
@@ -45,15 +52,28 @@ export class StandardComponent {
     }
   }
 
+  //appends the decimal to the number
   appendDecimal(){
+    if(this.operators.includes(this.displayValue)) return;
+
+    if(this.displayClear){
+      this.displayClear = false;
+    }
+
+    if(!this.decimal){
+      this.displayValue += '.';
+      this.decimal = true;
+    }
   }
 
+  //set the operator into the display line
   setOperator(operator: string){
     if(this.operator != null){
       return;
     }
     if(this.firstValue == null){
       this.firstValue = this.displayValue;
+      this.decimal = false;
       this.displayClear = true;
     }
     if(this.displayClear){
@@ -61,11 +81,18 @@ export class StandardComponent {
     }
   }
 
+
+  //executes when the equals button is pressed
   evaluate(){
+    //does nothing if a valid equation can not be built
     if(this.firstValue == null || this.operator == null){
       return;
     }
+
+    //sets the second value with the display if a valid equation can be built
     this.secondValue = this.displayValue;
+
+    //checks what operator was passed and evaluates the right equation on the first value and second value
     if(this.operator == '+'){
       this.answer = parseFloat(this.firstValue) + parseFloat(this.secondValue);
       this.displayValue = this.answer.toString();
